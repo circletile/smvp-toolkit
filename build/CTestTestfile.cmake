c6d81548f5f7ba6719317a39c73c30e233222dea
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-05 22:26:15 -0500

    Create base file tree.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cupchurch/git-repos/smvp-csr
+# Build directory: /home/cupchurch/git-repos/smvp-csr/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
