7e04a035a33656a5dadf13ac2af4ff0f1b7ee97c
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-06 16:27:20 -0500

    Done with mmio tests.

--- main.c
+++ main.c
@@ -10,4 +10,20 @@
 
 int main(int argc, char *argv[])
 {
+
+    // 1. Load the sparse matrix file
+
+    // 2. Load the vector to be multiplied
+    
+    // 3*. Visual representation of loaded matrix and vector (*if time)
+
+    // 4. DO THE THING (y=(A^n)*x using CSR)
+        // a. setup logging of overall compute time
+        // b. setup logging of per iteration compute time
+        // c. compute the product
+
+    // 5*. Visual representation of output product (*if time)
+
+    // 6. Write compute and execution time results to file(s)
+
 }
