f3dde22e0c620f3c17598bf2800391f5b37012ec
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-06 00:44:38 -0500

    Test hello world equivalent with cmake.

---
+++ main.c
@@ -0,0 +1,9 @@
+// first.c -- a simple first C program
+/* compile with:cc -g -o first first.c*/
+#include <stdio.h>      // for printf()
+#include <math.h>       // for cos()
+
+int main (int argc, char *argv[]){
+    printf ("the cosine of 1 is %g\n", cos(1.0));
+    printf ("thank you, and have a nice day\n");
+} //main
