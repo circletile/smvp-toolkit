1147f19f00819120a5bb4b428747ac1a67937fff
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-06 17:15:36 -0500

    Add input file checks, terminal output coloring.

--- main.c
+++ main.c
@@ -8,22 +8,50 @@
 #include <stdlib.h>
 #include "mmio.h"
 
+// ANSI terminal color escape codes for making printf BEAUTIFUL
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 int main(int argc, char *argv[])
 {
+
+    FILE *mmInputFile, *mmOutputFile;
+    MM_typecode matcode;
+
+    // Validate user provided arguments, handle exceptions accordingly
+    if (argc < 2)
+    {
+        fprintf(stderr, ANSI_COLOR_YELLOW   "Usage: %s [martix-market-filename]\n"  ANSI_COLOR_RESET, argv[0]);
+        exit(1);
+    }
+    else
+    {
+        if ((mmInputFile = fopen(argv[1], "r")) == NULL)
+            exit(1);
+    }
+    if (mm_read_banner(mmInputFile, &matcode) != 0)
+    {
+        printf(ANSI_COLOR_RED   "Could not process Matrix Market file.\nEnsure file is correctly formatted and does not use gz/zip compression.\n" ANSI_COLOR_RESET);
+        exit(1);
+    }
 
     // 1. Load the sparse matrix file
 
     // 2. Load the vector to be multiplied
-    
+
     // 3*. Visual representation of loaded matrix and vector (*if time)
 
     // 4. DO THE THING (y=(A^n)*x using CSR)
-        // a. setup logging of overall compute time
-        // b. setup logging of per iteration compute time
-        // c. compute the product
+    // a. setup logging of overall compute time
+    // b. setup logging of per iteration compute time
+    // c. compute the product
 
     // 5*. Visual representation of output product (*if time)
 
     // 6. Write compute and execution time results to file(s)
-
 }
