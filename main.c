21b2dd1d2cd5780570e550d419d844d5bb5318bf
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-06 21:32:18 -0500

    Add file input parse error handlers.

--- main.c
+++ main.c
@@ -9,24 +9,25 @@
 #include "mmio.h"
 
 // ANSI terminal color escape codes for making printf BEAUTIFUL
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 int main(int argc, char *argv[])
 {
 
     FILE *mmInputFile, *mmOutputFile;
     MM_typecode matcode;
+    int mmio_return;
 
     // Validate user provided arguments, handle exceptions accordingly
     if (argc < 2)
     {
-        fprintf(stderr, ANSI_COLOR_YELLOW   "Usage: %s [martix-market-filename]\n"  ANSI_COLOR_RESET, argv[0]);
+        fprintf(stderr, ANSI_COLOR_YELLOW "Usage: %s [martix-market-filename]\n" ANSI_COLOR_RESET, argv[0]);
         exit(1);
     }
     else
@@ -34,10 +35,38 @@ int main(int argc, char *argv[])
         if ((mmInputFile = fopen(argv[1], "r")) == NULL)
             exit(1);
     }
-    if (mm_read_banner(mmInputFile, &matcode) != 0)
+
+    // Ensure file is of proper format and content
+    mmio_return = mm_read_banner(mmInputFile, &matcode);
+    if (mmio_return != 0)
     {
-        printf(ANSI_COLOR_RED   "Could not process Matrix Market file.\nEnsure file is correctly formatted and does not use gz/zip compression.\n" ANSI_COLOR_RESET);
-        exit(1);
+        if (mmio_return == MM_PREMATURE_EOF)
+        {
+            printf(ANSI_COLOR_RED "Could not process specified Matrix Market input file.\nRequired items not present on first line of file.\n" ANSI_COLOR_RESET);
+            exit(1);
+        }
+        else if (mmio_return == MM_NO_HEADER)
+        {
+            printf(ANSI_COLOR_RED "Could not process specified Matrix Market input file.\nRequired header is missing or file contents may not be Matrix Market formatted.\n" ANSI_COLOR_RESET);
+            exit(1);
+        }
+        else if (mmio_return == MM_UNSUPPORTED_TYPE)
+        {
+            printf(ANSI_COLOR_RED "Could not process specified Matrix Market input file.\nMatrix content description not parseable or is absent.\n" ANSI_COLOR_RESET);
+            exit(1);
+        }
+        else{
+            printf(ANSI_COLOR_RED "Could not process specified Matrix Market input file.\nUnhandled exception occured during file loading .\n" ANSI_COLOR_RESET);
+            exit(1);
+        }
+    }
+    else
+    {
+        if (mm_is_sparse(matcode) == 0)
+        {
+            printf(ANSI_COLOR_RED "This application only supports sparse matricies.\nSpecified input file does not appear to contain a sparse matrix.\n" ANSI_COLOR_RESET);
+            exit(1);
+        }
     }
 
     // 1. Load the sparse matrix file
