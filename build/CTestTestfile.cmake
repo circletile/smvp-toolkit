3bbfa40a58914c9cfd2d0e99b6cf1d10983742b4
Chris Upchurch <{ID}+{username}@users.noreply.github.com>
2021-02-07 16:28:09 -0500

    Remove newly ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cupchurch/git-repos/smvp-csr
-# Build directory: /home/cupchurch/git-repos/smvp-csr/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
